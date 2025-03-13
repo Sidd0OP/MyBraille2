2af8e34691cd8052d4b70cb972c7a40f5732eb90
Siddharth16529 <siddharthmj0360@gmail.com>
2025-03-13 16:41:18 +0530

    Implementing touch and gesture navigation

--- app/src/main/java/com/example/mybraille/MainActivity.java
+++ app/src/main/java/com/example/mybraille/MainActivity.java
@@ -1,21 +1,54 @@
 package com.example.mybraille;
 
+
 import android.os.Bundle;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.View;
 import android.view.Window;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
 
 
+    //main method for android
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
 
+        //hide top menue bar
         getSupportActionBar().hide();
+
+
 
         setContentView(R.layout.activity_main);
 
+
+        View bottomBar = findViewById(R.id.bottomBar);
+
+
+
+        bottomBar.setOnTouchListener(new View.OnTouchListener() {
+
+            @Override
+            public boolean onTouch(View v, MotionEvent e) {
+
+                if (e.getAction() == MotionEvent.ACTION_DOWN ) {
+                    System.out.println("Inside Bottom Bar");
+                    return true;
+                }
+
+                return false;
+            }
+        });
+
     }
+
+
+
 }
+
